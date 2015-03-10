8d3d60ef263719bd636da75599a15d1f810a0ede
Simon S. Soriano <simon0191@gmail.com>
2015-03-09 19:56:34 -0500

    Add test to hola_mundo

--- lib/ri00_hola_mundo.rb
+++ lib/ri00_hola_mundo.rb
@@ -1,7 +1,1 @@
-=begin
-  Bienvenido a Intro a Ruby.
-  Así puedes escribir comentarios multi-línea
-=end
-# Así puedes escribir comentarios de una sola línea
-# Y así puedes imprimir un texto por consola:
-puts "Hello world!"
+# Imprime el texto 'Hola Mundo!'
---
+++ spec/lib/r00_hola_mundo_spec.rb
@@ -0,0 +1,11 @@
+
+RSpec.describe "ri00_hola_mundo" do
+
+  it "should call puts with string 'Hola Mundo!'" do
+
+    expect(STDOUT).to receive(:puts).with("Hola Mundo!")
+    require "ri00_hola_mundo"
+    
+  end
+  
+end
