3ddde48271ca9e2fdc4e0f429050e349f344efc0
Simon S. Soriano <simon0191@gmail.com>
2015-03-09 21:33:32 -0500

    Add ri02

---
+++ lib/ri02_metodos.rb
@@ -0,0 +1,19 @@
+# Define el método saludar. Debe recibir un parámetro y retornar un string
+# como se muestra en el ejemplo:
+#
+# Ejemplo: saludar("Simón") => "Hola Simón"
+
+
+# Define el método sumar que recibe 2 números e imprime su suma.
+#
+# Ejemplo: sumar(4,8) => 12
+
+
+
+# Define el método sumar_varios que recibe un número no determinado
+# de parametros e imprime su suma.
+# (*) Google lo sabe todo ;)
+#
+# Ejemplo: sumar_varios(4,8) => 12
+# Ejemplo: sumar_varios(4,8,3,5) => 20
+# Ejemplo: sumar_varios(1) => 1
---
+++ spec/lib/ri02_metodos_spec.rb
@@ -0,0 +1,24 @@
+require "ri02_metodos"
+
+RSpec.describe "ri01_tipos_de_dato" do
+
+  it "should define saludar" do
+    expect(defined? saludar).to_not eq(nil)
+    expect(saludar("Simón")).to eq("Hola Simón")
+  end
+
+  it "should define sumar" do
+    expect(defined? sumar).to_not eq(nil)
+    expect(sumar(3,2)).to eq(3+2)
+    expect(sumar(-5,8)).to eq(-5+8)
+    expect(sumar(0,0)).to eq(0)
+  end
+
+  it "should define sumar_varios" do
+    expect(defined? sumar_varios).to_not eq(nil)
+    expect(sumar_varios(3,2)).to eq(5)
+    expect(sumar_varios(1,1,1,1)).to eq(4)
+    expect(sumar_varios(0,0,0,0,0,0,0,0,0,0)).to eq(0)
+    expect(sumar_varios(1)).to eq(1)
+  end
+end
