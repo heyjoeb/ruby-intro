75fc380fd6d732e630ca147c218d63749323cb16
Simon S. Soriano <simon0191@gmail.com>
2015-03-09 21:34:23 -0500

    Add solution to ri02

--- lib/ri02_metodos.rb
+++ lib/ri02_metodos.rb
@@ -2,13 +2,17 @@
 # como se muestra en el ejemplo:
 #
 # Ejemplo: saludar("Simón") => "Hola Simón"
-
+def saludar(name)
+  "Hola #{name}"
+end
 
 # TODO: Define el método sumar que recibe 2 números e imprime su suma.
 #
 # Ejemplo: sumar(4,8) => 12
 
-
+def sumar(a,b)
+  a+b
+end
 
 # TODO: Define el método sumar_varios que recibe un número no determinado
 # de parametros e imprime su suma.
@@ -17,12 +21,19 @@
 # Ejemplo: sumar_varios(4,8,3,5) => 20
 # Ejemplo: sumar_varios(1) => 1
 
-
+def sumar_varios(*nums)
+  sum = 0
+  nums.each { |n| sum+=n }
+  sum
+end
 
 # TODO: Define el método reversar_string
 #
 # Ejemplo: reversar_string("hola mundo") => "odnum aloh"
 
+def reversar_string(s)
+  s.reverse
+end
 
 # TODO: Define el método es_palindrome?
 #
@@ -31,11 +42,19 @@
 # Ejemplo: es_palindrome?("aabbaa") => true
 # Ejemplo: es_palindrome?("anita lava la tina") => true
 
+def es_palindrome?(s)
+  s.gsub(" ","") == s.gsub(" ","").reverse
+end
+
 # TODO: Define el método reversar_palabras
 #
 # Ejemplo: reversar_palabras("hola    mundo   ") => "aloh odnum"
 # Ejemplo: reversar_palabras("curso de ruby on rails") => "osruc ed ybur no sliar"
 # Ejemplo: reversar_palabras("  hola  ") => "aloh"
+
+def reversar_palabras(s)
+  s.split.map(&:reverse).join(" ")
+end
 
 # TODO: Define el método invertir_palabras
 #
@@ -43,8 +62,9 @@
 # Ejemplo: invertir_palabras("curso de ruby on rails") => "rails on ruby de curso"
 # Ejemplo: invertir_palabras("hola") => "hola"
 
-
-
+def invertir_palabras(s)
+  s.split.reverse.join(" ")
+end
 
 # El código que escribas dentro de esta condición no será ejecutado en las
 # pruebas.
